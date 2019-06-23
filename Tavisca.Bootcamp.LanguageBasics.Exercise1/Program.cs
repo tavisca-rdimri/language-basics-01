567435b94709c77bd6929bc52c9d97c3de137986
tavisca-rdimri <rdimri@tavisca.in>
2019-06-24 00:29:46 +0530

    This is final commit for code 1

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -20,9 +20,111 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
             Console.WriteLine($"{args} : {result}");
         }
 
-        public static int FindDigit(string equation)
+        public static int FindDigit(string str)
         {
-            // Add your code here.
+            string a=string.Empty;
+		    string b=string.Empty;
+		    string c=string.Empty;
+		    int j=0,k=0,l=0,i,ai,bi,ci,z=0;
+			int len=str.Length;
+		    for(i=0;i<len;i++)
+		    {
+		        if(z==0)
+		        {
+					if(str[i]!='*')
+		            a+=str[i];
+		        }
+		        else if(z==1)
+		        {
+					if(str[i]!='=')
+		            b+=str[i];
+		        }
+		        else if(z==2)
+		        {
+		            c+=str[i];
+		        }
+		        if(str[i]=='*')
+		        {
+		           j=i;
+		           z++;
+		        }
+		        else if(str[i]=='?')
+		        {
+		            l=i;
+		        }
+		        else if(str[i]=='=')
+		        {
+		            k=i;
+		            z++;
+		        }
+		    }
+		
+		        if(l<j)
+		    {
+		        bi=Convert.ToInt32(b);
+		        ci=Convert.ToInt32(c);
+		        for(i=0;i<10;i++)
+		        {
+					string s=string.Empty;
+		            if(a[0]=='?'&&i==0)
+		              continue;
+		            foreach(char x in a)
+					{
+						if(x!='?')
+						s+=x;
+						else
+							s+=i;
+					}
+					ai=Convert.ToInt32(s);
+					if(ai*bi==ci)
+		             return i;
+		        }
+		        return -1;
+		    }
+		    else if(l>k)
+		    {
+		         bi=Convert.ToInt32(b);
+		         ai=Convert.ToInt32(a);
+		        for(i=0;i<10;i++)
+		        {
+					string s=string.Empty;
+		            if(c[0]=='?'&&i==0)
+		              continue;
+		            foreach(char x in c)
+					{
+						if(x!='?')
+						s+=x;
+						else
+							s+=i;
+					}
+					ci=Convert.ToInt32(s);
+					if(ai*bi==ci)
+		             return i;
+		        }
+		        return -1;
+		    }
+		    else
+		    {
+		         ci=Convert.ToInt32(c);
+		         ai=Convert.ToInt32(a);
+		        for(i=0;i<10;i++)
+		        {
+					string s=string.Empty;
+		            if(b[0]=='?'&&i==0)
+		              continue;
+		            foreach(char x in b)
+					{
+						if(x!='?')
+						s+=x;
+						else
+							s+=i;
+					}
+					bi=Convert.ToInt32(s);
+		           	if(ai*bi==ci)
+		             return i;
+		        }
+		        return -1;   
+		    }            
             throw new NotImplementedException();
         }
     }
