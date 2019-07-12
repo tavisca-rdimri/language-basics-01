d46ed90782c250acad13badb1c23b7cd72332438
tavisca-rdimri <rdimri@tavisca.in>
2019-07-12 11:50:20 +0530

    Final code

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -19,112 +19,98 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
             var result = FindDigit(args).Equals(expected) ? "PASS" : "FAIL";
             Console.WriteLine($"{args} : {result}");
         }
-
-        public static int FindDigit(string str)
+        
+        public static int FindDigit(string expression)
         {
-            string a=string.Empty;
-		    string b=string.Empty;
-		    string c=string.Empty;
-		    int j=0,k=0,l=0,i,ai,bi,ci,z=0;
-			int len=str.Length;
-		    for(i=0;i<len;i++)
+            string firstOperand=string.Empty;
+		    string secondOperand = string.Empty;
+		    string thirdOperand = string.Empty;
+		    int multiplicationIndex=0, equalIndex = 0, questionIndex = 0,firstOperandInt,secondOperandInt,thirdOperandInt, operand=0;
+		    for(int i=0;i< expression.Length; i++)
 		    {
-		        if(z==0)
+		        if(operand == 0)
 		        {
-					if(str[i]!='*')
-		            a+=str[i];
+					if(expression[i]!='*')
+                        firstOperand += expression[i];
 		        }
-		        else if(z==1)
+		        else if(operand == 1)
 		        {
-					if(str[i]!='=')
-		            b+=str[i];
+					if(expression[i]!='=')
+                        secondOperand += expression[i];
 		        }
-		        else if(z==2)
+		        else if(operand == 2)
 		        {
-		            c+=str[i];
+                    thirdOperand += expression[i];
 		        }
-		        if(str[i]=='*')
+		        if(expression[i]=='*')
 		        {
-		           j=i;
-		           z++;
+                    multiplicationIndex = i;
+		            operand++;
 		        }
-		        else if(str[i]=='?')
+		        else if(expression[i]=='?')
 		        {
-		            l=i;
+                    questionIndex = i;
 		        }
-		        else if(str[i]=='=')
+		        else if(expression[i]=='=')
 		        {
-		            k=i;
-		            z++;
+                    equalIndex = i;
+		            operand++;
 		        }
 		    }
-		
-		        if(l<j)
+
+            if (questionIndex > equalIndex)
+            {
+                secondOperandInt = Convert.ToInt32(secondOperand);
+                firstOperandInt = Convert.ToInt32(firstOperand);
+                for (int i = 0; i < 10; i++)
+                {
+                    string thirdOperandTemporary = string.Empty;
+                    if (thirdOperand[0] == '?' && i == 0)
+                        continue;
+                    foreach (char x in thirdOperand)
+                    {
+                        if (x != '?')
+                            thirdOperandTemporary += x;
+                        else
+                            thirdOperandTemporary += i;
+                    }
+                    thirdOperandInt = Convert.ToInt32(thirdOperandTemporary);
+                    if (firstOperandInt * secondOperandInt == thirdOperandInt)
+                        return i;
+                }
+                return -1;
+            }
+            else
 		    {
-		        bi=Convert.ToInt32(b);
-		        ci=Convert.ToInt32(c);
-		        for(i=0;i<10;i++)
+                if (questionIndex < multiplicationIndex)
+                {
+                    secondOperandInt = Convert.ToInt32(secondOperand);
+                    thirdOperandInt = Convert.ToInt32(thirdOperand);
+                }
+                else
+                {
+                    secondOperandInt = Convert.ToInt32(firstOperand);
+                    thirdOperandInt = Convert.ToInt32(thirdOperand);
+                    firstOperand = secondOperand;
+                }
+		        for(int i=0;i<10;i++)
 		        {
-					string s=string.Empty;
-		            if(a[0]=='?'&&i==0)
+					string firstOperandTemporary = string.Empty;
+		            if(firstOperand[0]=='?'&&i==0)
 		              continue;
-		            foreach(char x in a)
+		            foreach(char x in firstOperand)
 					{
 						if(x!='?')
-						s+=x;
+                            firstOperandTemporary += x;
 						else
-							s+=i;
+                            firstOperandTemporary += i;
 					}
-					ai=Convert.ToInt32(s);
-					if(ai*bi==ci)
+                    firstOperandInt= Convert.ToInt32(firstOperandTemporary);
+					if(firstOperandInt * secondOperandInt == thirdOperand)
 		             return i;
 		        }
 		        return -1;
 		    }
-		    else if(l>k)
-		    {
-		         bi=Convert.ToInt32(b);
-		         ai=Convert.ToInt32(a);
-		        for(i=0;i<10;i++)
-		        {
-					string s=string.Empty;
-		            if(c[0]=='?'&&i==0)
-		              continue;
-		            foreach(char x in c)
-					{
-						if(x!='?')
-						s+=x;
-						else
-							s+=i;
-					}
-					ci=Convert.ToInt32(s);
-					if(ai*bi==ci)
-		             return i;
-		        }
-		        return -1;
-		    }
-		    else
-		    {
-		         ci=Convert.ToInt32(c);
-		         ai=Convert.ToInt32(a);
-		        for(i=0;i<10;i++)
-		        {
-					string s=string.Empty;
-		            if(b[0]=='?'&&i==0)
-		              continue;
-		            foreach(char x in b)
-					{
-						if(x!='?')
-						s+=x;
-						else
-							s+=i;
-					}
-					bi=Convert.ToInt32(s);
-		           	if(ai*bi==ci)
-		             return i;
-		        }
-		        return -1;   
-		    }            
             throw new NotImplementedException();
         }
     }
